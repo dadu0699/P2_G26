8fa26b45242d25e5f172f85747df71e0409dd6fa
Didier Domínguez <dadu0699@hotmail.com>
2021-04-01 00:18:00 -0600

    [ADD] Diseño pagina libros :art:
    Co-Authored-By: Jacqueline Méndez <54610954+Jacqueline06@users.noreply.github.com>

---
+++ Books-Library/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const indexRouter = require('./routes/index.route');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+app.use(express.urlencoded({
+    // application/x-www-form-urlencoded
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes 
+app.use('/', indexRouter);
+
+app.use('/uploads', express.static(path.resolve('uploads')));
+
+// Port assignment
+app.listen(PORT, () => {
+    console.log(`App listening on port ${ PORT }`);
+});
+
+// Catch 404
+app.use((req, res, next) => {
+    res.status(404).send({
+        Error: 404,
+        Descripcion: 'Page not found'
+    });
+    // res.status(404).sendFile(__dirname + "/public/404.html"); // next(createError(404));
+});
