a3262720ec828e901fd217024584c89052b39515
Didier Domínguez <dadu0699@hotmail.com>
2021-04-01 21:27:52 -0600

    [UPDATE] Backend

--- Books-Library/routes/index.route.js
+++ Books-Library/routes/index.route.js
@@ -5,11 +5,6 @@ const multer = require('../config/multer');
 const bookController = require('../controllers/book.controller');
 
 router.route('/').get(bookController.get);
-
-router.route('/book')
-    .get((req, res) => {
-        res.render('book');
-    })
-    .post(multer.single('image'), bookController.create);
+router.route('/book').post(multer.single('image'), bookController.create);
 
 module.exports = router;
