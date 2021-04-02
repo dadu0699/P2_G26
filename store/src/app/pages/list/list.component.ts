966a2da796e4167a45e955ebe6b4fe3aaf0d0445
Didier Domínguez <dadu0699@hotmail.com>
2021-04-01 23:12:55 -0600

    [ADD] Frontend 🛠️

---
+++ store/src/app/pages/list/list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Global } from 'src/app/services/global';
+import { Book } from 'src/app/models/book';
+import { BooksService } from 'src/app/services/books.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  public url: string;
+  public books: Book[];
+
+  constructor(
+    private _booksService: BooksService
+  ) {
+    this.url = `${Global.url}/`;
+    this.books = [];
+  }
+
+  async ngOnInit(): Promise<void> {
+    await this.getData();
+  }
+
+  private async getData(): Promise<void> {
+    try {
+      const data = await this._booksService.getAll();
+      if (data['code'] === '200') {
+        this.books = data['data'];
+      }
+    } catch (err) {
+      console.log(<any>err);
+    }
+  }
+}
